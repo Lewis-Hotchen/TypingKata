df2071e57b4f4037c46d69da241cb7d7f90f2484
Lewis-Hotchen <tranderta@gmail.com>
2021-01-29 11:27:51 +0000

    Added class for word queues

---
+++ TypingKata/KataSpeedProfilerModule/IWordQueue.cs
@@ -0,0 +1,41 @@
+﻿namespace KataSpeedProfilerModule {
+
+    /// <summary>
+    /// Simple interface that wraps a C# Queue.
+    /// </summary>
+    public interface IWordQueue {
+
+        /// <summary>
+        /// The top of the queue, or first in queue.
+        /// </summary>
+         IWord Top { get; }
+
+        /// <summary>
+        /// The count of words in the queue.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Get the queue as an array.
+        /// </summary>
+        /// <returns></returns>
+        IWord[] GetWordQueueAsArray();
+
+        /// <summary>
+        /// Clear the queue.
+        /// </summary>
+        void ClearQueue();
+
+        /// <summary>
+        /// Add word to the queue.
+        /// </summary>
+        /// <param name="word">The word to be added.</param>
+        void Enqueue(IWord word);
+
+        /// <summary>
+        /// Remove word from the queue.
+        /// </summary>
+        /// <returns>The word to be removed.</returns>
+        IWord Dequeue();
+    }
+}
---
+++ TypingKata/SpeedProfilerUnitTests/WordQueueUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using KataSpeedProfilerModule;
+using Moq;
+using NUnit.Framework;
+
+namespace SpeedProfilerUnitTests {
+
+    [TestFixture]
+    public class WordQueueUnitTest {
+        private Mock<IWord> _mockWord;
+
+        [SetUp]
+        public void Setup() {
+            _mockWord = new Mock<IWord>();
+            _mockWord.Setup(w => w.Chars).Returns(new[] {'t', 'e', 's', 't'});
+        }
+
+        [Test]
+        public void ShouldQueueNewWord() {
+            var target = CreateTarget();
+            var expected = new[] {'t', 'e', 's', 't'};
+            target.Enqueue(_mockWord.Object);
+
+            Assert.AreEqual(expected, target.Top.Chars);
+        }
+
+        [Test]
+        public void ShouldDequeueWord() {
+            var target = CreateTarget();
+            target.Enqueue(_mockWord.Object);
+            target.Dequeue();
+
+            Assert.AreEqual(0, target.Count);
+        }
+
+        public WordQueue CreateTarget() {
+            return new WordQueue();
+        }
+    }
+}
