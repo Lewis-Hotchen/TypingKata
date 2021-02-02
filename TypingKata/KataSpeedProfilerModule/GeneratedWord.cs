72fb0a2c2fd5b61d46ae1b83415084c397f73b1d
Lewis-Hotchen <tranderta@gmail.com>
2021-02-02 10:04:20 +0000

    Added new word class to facilitate generated words and user words. Added data structures to facilitate both user words and generated words collections

---
+++ TypingKata/KataSpeedProfilerModule/GeneratedWord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace KataSpeedProfilerModule {
+    public class GeneratedWord : IWord {
+        public IList<char> Chars { get; }
+        public int CharCount => Chars.Count();
+
+        public GeneratedWord(string word) {
+            Chars = word.ToCharArray();
+        }
+
+        public override string ToString() {
+            return new string(Chars.ToArray());
+        }
+    }
+}
---
+++ TypingKata/KataSpeedProfilerModule/UserDefinedWord.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KataSpeedProfilerModule {
+    class UserDefinedWord : IWord {
+        public IList<char> Chars { get; }
+        public int CharCount { get; }
+
+        public override string ToString() {
+            return new string(Chars.ToArray());
+        }
+
+        public UserDefinedWord() {
+        }
+    }
+}
